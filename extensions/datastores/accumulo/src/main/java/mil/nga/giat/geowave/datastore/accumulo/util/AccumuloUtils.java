082464d352197684103881353a5ad62df1bc0558
Derek Yeager <dcy2003@gmail.com>
2015-12-18 14:48:43 -0500

    wip: hacky test

--- extensions/datastores/accumulo/src/main/java/mil/nga/giat/geowave/datastore/accumulo/util/AccumuloUtils.java
+++ extensions/datastores/accumulo/src/main/java/mil/nga/giat/geowave/datastore/accumulo/util/AccumuloUtils.java
@@ -308,7 +308,8 @@ public class AccumuloUtils
 				final List<FieldInfo<Object>> fieldInfos = decomposeFlattenedFields(
 						indexModel,
 						byteValue,
-						entry.getKey().getColumnVisibilityData().getBackingArray());
+						entry.getKey().getColumnVisibilityData().getBackingArray(),
+						adapter);
 				for (final FieldInfo<Object> fieldInfo : fieldInfos) {
 					final FieldReader<? extends CommonIndexValue> indexFieldReader = indexModel.getReader(fieldInfo.getDataValue().getId());
 					if (indexFieldReader != null) {
@@ -603,17 +604,14 @@ public class AccumuloUtils
 			final List<byte[]> fieldInfoBytesList = new ArrayList<>();
 			int totalLength = 0;
 			for (final FieldInfo<?> fieldInfo : entry.getValue()) {
-				final byte[] fieldIdBytes = fieldInfo.getDataValue().getId().getBytes();
-				final ByteBuffer fieldInfoBytes = ByteBuffer.allocate(8 + fieldIdBytes.length + fieldInfo.getWrittenValue().length);
-				fieldInfoBytes.putInt(fieldIdBytes.length);
-				fieldInfoBytes.put(fieldIdBytes);
+				final ByteBuffer fieldInfoBytes = ByteBuffer.allocate(4 + fieldInfo.getWrittenValue().length);
 				fieldInfoBytes.putInt(fieldInfo.getWrittenValue().length);
 				fieldInfoBytes.put(fieldInfo.getWrittenValue());
 				fieldInfoBytesList.add(fieldInfoBytes.array());
 				totalLength += fieldInfoBytes.array().length;
 			}
-			final ByteBuffer allFields = ByteBuffer.allocate(4 + totalLength);
-			allFields.putInt(entry.getValue().size());
+			final ByteBuffer allFields = ByteBuffer.allocate(totalLength);
+			// allFields.putInt(entry.getValue().size());
 			for (final byte[] bytes : fieldInfoBytesList) {
 				allFields.put(bytes);
 			}
@@ -643,27 +641,20 @@ public class AccumuloUtils
 	public static <T> List<FieldInfo<Object>> decomposeFlattenedFields(
 			final CommonIndexModel model,
 			final byte[] flattenedValue,
-			final byte[] commonVisibility ) {
+			final byte[] commonVisibility,
+			final DataAdapter<T> dataAdapter ) {
 		final List<FieldInfo<Object>> fieldInfoList = new ArrayList<>();
 		final ByteBuffer input = ByteBuffer.wrap(flattenedValue);
-		final int numFields = input.getInt();
-		for (int x = 0; x < numFields; x++) {
-			final int fieldIdLength = input.getInt();
-			final byte[] fieldIdBytes = new byte[fieldIdLength];
-			input.get(fieldIdBytes);
+		// final int numFields = input.getInt();
+		List<ByteArrayId> ids = ((AbstractDataAdapter<?>) dataAdapter).getOrderedFields(model);
+		for (ByteArrayId fieldId : ids) {
 			final int fieldLength = input.getInt();
 			final byte[] fieldValueBytes = new byte[fieldLength];
 			input.get(fieldValueBytes);
-			final ByteArrayId fieldId = new ByteArrayId(
-					fieldIdBytes);
-			final FieldReader<? extends CommonIndexValue> fieldReader = model.getReader(fieldId);
-			Object fieldValue = null;
-			if (fieldReader != null) {
-				fieldValue = fieldReader.readField(fieldValueBytes);
-			}
+			model.getDimensions();
 			final PersistentValue<Object> persistentValue = new PersistentValue<Object>(
 					fieldId,
-					fieldValue);
+					null);
 			final FieldInfo<Object> fieldInfo = DataStoreUtils.getFieldInfo(
 					persistentValue,
 					fieldValueBytes,
